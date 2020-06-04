26ca3c9770a5ab7bc5d8bf946af951338e745978
cabosh <callum.bosher@gmail.com>
2020-06-04 23:34:36 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
 
+    amount = 0
+    total = 0
+
+    while True:
+
+        number = int(input("Enter a number: "))
+
+        if number > 0:
+            amount = amount + 1
+            total = total + number
+        elif number < 0:
+            amount = amount + 1
+            total = total + number
+        elif number == 0:
+            print(f"The number of numbers is: {amount}.")
+            print(f"The sum of the numbers is: {total}")
+            break
+
 if __name__ == '__main__':
     main()
